ae6f3f620f2467a415c791b1c952f28d6e7ef451
TestAccountIdk-1 <22hannedw@reigategrammar.org>
2025-10-09 10:54:26 +0100

    Add files via upload

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,10 +2,10 @@ function dir-hFUNC {get-childitem -Force}
 set-alias -name dir-h -value dir-hFUNC
 
 function cmdFUNC {write-warning 'READ ALL OF THE FOLLOWING:'
-get-content C:\Users\Public\Downloads\warning.txt
+write-output "I have not tested this command. I do not know what the consequences will be, but you have to accept that I am not responsible for them. By proceeding with this action, you accept total responsibility! To stop the command, say 'No to all' or just close Terminal. Please use responsibly."
 write-output "The command continues 30 seconds after the warning has printed."
 start-sleep 30
-del C:\Program Files\google\chrome\Application\140.0.7339.208\default_apps\external_extensions.json -Confirm -Force
-del C:\Program Files\Smoothwall -Confirm -Force
+del "C:\Program Files\google\chrome\Application\140.0.7339.208\default_apps\external_extensions.json" -Confirm -Force
+del "C:\Program Files\Smoothwall" -Confirm -Force
 }
 set-alias kill-wall -value cmdFUNC
